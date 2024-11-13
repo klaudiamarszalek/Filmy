aec4772825e98f2bb222e4c36d1120d272b77208
klaudia_marszalek <klaudia.marszalek.04@gmail.com>
2024-11-13 21:11:37 +0100

    Dodaj pliki projektów.

---
+++ WebApplication5/Controllers/FilmController.cs
@@ -0,0 +1,77 @@
+﻿using WebApplication5.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace WebApplication5.Controllers
+{
+    public class FilmController : Controller
+    {
+        private static IList<Film> films = new List<Film>
+        {
+            new Film(){Id = 1, Name = "film1", Description = "desc1", Price = 2},
+            new Film(){Id = 2, Name = "film2", Description = "desc2", Price = 2},
+            new Film(){Id = 3, Name = "film3", Description = "desc3", Price = 2},
+            new Film(){Id = 4, Name = "film4", Description = "desc4", Price = 2}
+        };
+        // GET: FilmController
+        public ActionResult Index()
+        {
+            return View(films);
+        }
+
+        // GET: FilmController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(films.FirstOrDefault(x => x.Id == id));
+        }
+
+        // GET: FilmController/Create
+        public ActionResult Create()
+        {
+            return View(new Film());
+        }
+
+        // POST: FilmController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Film film)
+        {
+            film.Id = films.Count+1;
+            films.Add(film);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: FilmController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FilmController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Film film)
+        {
+            films[id].Name = film.Name;
+            films[id].Description = film.Description;
+            films[id].Price = film.Price;
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: FilmController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FilmController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Film film)
+        {
+            films.Remove(films[id]);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
